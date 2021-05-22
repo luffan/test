c6d532cde7756f4bd1041986f499ce09756c8271
luffan <mihalkovgleb@gmail.com>
2021-05-22 14:04:30 +0300

    future/ splitting into methods profile page

--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -32,128 +32,111 @@ class ProfilePage extends StatelessWidget {
             child: ListView(
               shrinkWrap: true,
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    ClipOval(
-                      child: Image.asset('assets/images/avatar.png', height: 114),
-                    ),
-                    SizedBox(height: 22),
-                    Text(
-                      Get.parameters['user'] ?? 'username',
-                      style: TextStyle(
-                        color: theme.primaryTextColor,
-                        fontSize: FontSize.fontSizeXL,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10, left: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'New York',
-                        style: TextStyle(
-                          color: theme.secondaryTextColor,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: Dimens.defaultVerticalMargin),
-                        child: Icon(
-                          Icons.circle,
-                          size: 6,
-                          color: const Color(0xFFB5C3C7),
-                        ),
-                      ),
-                      Text(
-                        'ID: 1120611',
-                        style: TextStyle(
-                          color: theme.secondaryTextColor,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 17, bottom: 24),
-                    child: Text(
-                      'Edit',
-                      style: TextStyle(
-                        color: theme.accentColor,
-                        decoration: TextDecoration.underline,
-                        fontWeight: FontWeight.w900,
-                        fontSize: FontSize.fontSizeM,
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: Dimens.defaultContentPadding),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: OutlinedButton(
-                          onPressed: () {},
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 22),
-                            child: Text(Strings.aboutMe),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 15),
-                      Expanded(
-                        child: AppButton.filledText(
-                          onClick: () {},
-                          text: Strings.logOut,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                _mainProfileData(),
+                _address(),
+                _editLabel(),
+                _clickButtons(),
                 SizedBox(height: 50),
-                Container(
-                  color: theme.primaryTextColor,
-                  padding: EdgeInsets.symmetric(vertical: 40),
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: _card(
-                          leading: Icons.phone,
-                          title: Strings.phoneNumber,
-                          content: '+375292756392',
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: _card(
-                          leading: Icons.mail,
-                          title: Strings.email,
-                          content: 'mihalkovgleb@gmail.com',
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: _card(
-                          leading: Icons.circle,
-                          title: Strings.completedProjects,
-                          content: '10',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                _additionalData(),
               ],
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _editLabel() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 17, bottom: 24),
+        child: Text(
+          'Edit',
+          style: TextStyle(
+            color: theme.accentColor,
+            decoration: TextDecoration.underline,
+            fontWeight: FontWeight.w900,
+            fontSize: FontSize.fontSizeM,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _mainProfileData() {
+    return Column(
+      children: <Widget>[
+        ClipOval(
+          child: Image.asset('assets/images/avatar.png', height: 114),
+        ),
+        SizedBox(height: 22),
+        Text(
+          Get.parameters['user'] ?? 'username',
+          style: TextStyle(
+            color: theme.primaryTextColor,
+            fontSize: FontSize.fontSizeXL,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _address() {
+    return Padding(
+      padding: EdgeInsets.only(top: 10, left: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'New York',
+            style: TextStyle(
+              color: theme.secondaryTextColor,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Dimens.defaultVerticalMargin),
+            child: Icon(
+              Icons.circle,
+              size: 6,
+              color: const Color(0xFFB5C3C7),
+            ),
+          ),
+          Text(
+            'ID: 1120611',
+            style: TextStyle(
+              color: theme.secondaryTextColor,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _clickButtons() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Dimens.defaultContentPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: OutlinedButton(
+              onPressed: () {},
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 22),
+                child: Text(Strings.aboutMe),
+              ),
+            ),
+          ),
+          SizedBox(width: 15),
+          Expanded(
+            child: AppButton.filledText(
+              onClick: () {},
+              text: Strings.logOut,
+            ),
+          )
         ],
       ),
     );
@@ -208,6 +191,43 @@ class ProfilePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _additionalData() {
+    return Container(
+      color: theme.primaryTextColor,
+      padding: EdgeInsets.symmetric(vertical: 40),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: _card(
+              leading: Icons.phone,
+              title: Strings.phoneNumber,
+              content: '+375292756392',
+            ),
+          ),
+          SizedBox(height: 16),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: _card(
+              leading: Icons.mail,
+              title: Strings.email,
+              content: 'mihalkovgleb@gmail.com',
+            ),
+          ),
+          SizedBox(height: 16),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: _card(
+              leading: Icons.circle,
+              title: Strings.completedProjects,
+              content: '10',
+            ),
+          ),
+        ],
       ),
     );
   }
